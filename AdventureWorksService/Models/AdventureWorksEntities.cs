cd303b01b21951f1370a7916335875630d3ae4be
Deng Yu <jardy@vip.qq.com>
2020-02-15 23:49:02 +0800

    添加项目文件。

---
+++ AdventureWorksService/Models/AdventureWorksEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdventureWorksService.Models
+{
+    public partial class QiubaoWorksEntities
+    {
+        public QiubaoWorksEntities(string password) : base("name=QiubaoWorksEntities")
+        {
+            this.Database.Connection.ConnectionString += $";Password={password}";
+        }
+    }
+}
